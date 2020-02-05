95a159903ab10b8b0e967acff04e5216db90df27
Jeff Rasley <jerasley@microsoft.com>
2020-02-05 11:05:46 -0800

    fix cifar run script

--- cifar/run_ds.sh
+++ cifar/run_ds.sh
@@ -4,6 +4,6 @@ num_gpus=1
 ds_config=ds_config.json
 
 args="--deepspeed --deepspeed_config ${ds_config}"
-run_cmd="deepspeed.pt --num_nodes 1 --num_gpus ${num_gpus} cifar10_deepspeed.py $@ ${args}"
+run_cmd="deepspeed --num_nodes 1 --num_gpus ${num_gpus} cifar10_deepspeed.py $@ ${args}"
 echo ${run_cmd}
 eval ${run_cmd}
