37f5c7899eb90658600bbbd6975c195867a4fb26
Elton Zheng <eltonz@microsoft.com>
2020-02-06 23:29:46 +0000

    simplify running command

--- cifar/run_ds.sh
+++ cifar/run_ds.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-num_gpus=1
-ds_config=ds_config.json
-
-args="--deepspeed --deepspeed_config ${ds_config}"
-run_cmd="deepspeed --num_nodes 1 --num_gpus ${num_gpus} cifar10_deepspeed.py $@ ${args}"
-echo ${run_cmd}
-eval ${run_cmd}
+deepspeed.pt cifar10_deepspeed.py --deepspeed --deepspeed_config ds_config.json
