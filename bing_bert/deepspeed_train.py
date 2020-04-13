8cf1be7deecd009e618fca06f03e0abf5661a4ae
Jeff Rasley <jerasley@microsoft.com>
2020-04-13 11:10:29 -0700

    copied from https://github.com/microsoft/AzureML-BERT/blob/dec79be13befdd51fa72c05419cf9288d32eb263/pretrain/PyTorch/azureml_adapter.py

--- bing_bert/deepspeed_train.py
+++ bing_bert/deepspeed_train.py
@@ -494,10 +494,33 @@ def _mpi_check(args):
                 os.environ['MASTER_ADDR'],
                 os.environ['MASTER_PORT']))
 
+def set_environment_variables_for_nccl_backend(single_node=False):
+    os.environ['RANK'] = os.environ['OMPI_COMM_WORLD_RANK']
+    os.environ['WORLD_SIZE'] = os.environ['OMPI_COMM_WORLD_SIZE']
+
+    if not single_node: 
+        master_node_params = os.environ['AZ_BATCH_MASTER_NODE'].split(':')
+        os.environ['MASTER_ADDR'] = master_node_params[0]
+        os.environ['MASTER_PORT'] = master_node_params[1]
+    else:
+        os.environ['MASTER_ADDR'] = os.environ['AZ_BATCHAI_MPI_MASTER_NODE']
+        os.environ['MASTER_PORT'] = '54965'
+    print('NCCL_SOCKET_IFNAME original value = {}'.format(os.environ['NCCL_SOCKET_IFNAME']))
+    # TODO make this parameterizable
+    os.environ['NCCL_SOCKET_IFNAME'] = '^docker0,lo'
+
+    print('RANK = {}'.format(os.environ['RANK']))
+    print('WORLD_SIZE = {}'.format(os.environ['WORLD_SIZE']))
+    print('MASTER_ADDR = {}'.format(os.environ['MASTER_ADDR']))
+    print('MASTER_PORT = {}'.format(os.environ['MASTER_PORT']))
+    print('NCCL_SOCKET_IFNAME new value = {}'.format(os.environ['NCCL_SOCKET_IFNAME']))
+
 def main():
     start = time.time()
     args = construct_arguments()
-    _mpi_check(args)
+    #_mpi_check(args)
+    set_environment_variables_for_nccl_backend()
+    args.local_rank = int(os.environ['OMPI_COMM_WORLD_LOCAL_RANK'])
     model, optimizer = prepare_model_optimizer(args)
     start_epoch = 0
     if not None in [args.load_training_checkpoint, args.load_checkpoint_id]:
