600eb8a37a2df5b76fd154eeb3f2b5358a57c1d1
Reza Yazdani <reyazda@microsoft.com>
2020-05-12 17:54:10 +0000

    fix 512 run

--- bing_bert/ds_train_bert_bsz32k_seq512.sh
+++ bing_bert/ds_train_bert_bsz32k_seq512.sh
@@ -17,7 +17,8 @@ NCCL_TREE_THRESHOLD=0 deepspeed ${base_dir}/deepspeed_train.py \
 --cf ${base_dir}/bert_large_lamb.json \
 --max_seq_length 512 \
 --output_dir $OUTPUT_DIR \
---print_steps 1 \
+--print_steps 100 \
+--deepspeed \
 --job_name $JOB_NAME \
 --deepspeed_config ${base_dir}/deepspeed_bsz32K_lamb_config_seq512.json \
 --data_path_prefix /data/bert \
