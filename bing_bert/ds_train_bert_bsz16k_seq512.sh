953981cfcaca7e17ca684f75ddc3e93a57a1a599
Jeff Rasley <jerasley@microsoft.com>
2020-05-12 00:34:36 -0700

    update config and run scripts

--- bing_bert/ds_train_bert_bsz16k_seq512.sh
+++ bing_bert/ds_train_bert_bsz16k_seq512.sh
@@ -3,11 +3,11 @@
 base_dir=`pwd`
 
 # Where should we save checkpoints and tensorboard events?
-JOB_NAME=lamb_32k_chkpt150_seq512_16_2
+JOB_NAME=lamb_32k_chkpt150_seq512
 OUTPUT_DIR=${base_dir}/bert_model_outputs
 
 # Assumes job name in previous seq128 run, will resume training from epoch 150
-CHECKPOINT_BASE_PATH=${OUTPUT_DIR}/saved_models/lamb_64k_seq128_64GPUs_1
+CHECKPOINT_BASE_PATH=${OUTPUT_DIR}/saved_models/lamb_64k_seq128
 CHECKPOINT_EPOCH150_NAME=`basename ${CHECKPOINT_BASE_PATH}/epoch150_*`
 echo "checkpoint id: $CHECKPOINT_EPOCH150_NAME"
 
@@ -19,9 +19,11 @@ NCCL_TREE_THRESHOLD=0 deepspeed ${base_dir}/deepspeed_train.py \
 --output_dir $OUTPUT_DIR \
 --print_steps 1 \
 --job_name $JOB_NAME \
---deepspeed_config ${base_dir}/deepspeed_bsz16K_lamb_config_seq512.json \
+--deepspeed_config ${base_dir}/deepspeed_bsz32K_lamb_config_seq512.json \
 --data_path_prefix /data/bert \
 --rewarmup \
+--lr_schedule "EE" \
+--lr_offset 0.0 \
 --load_training_checkpoint ${CHECKPOINT_BASE_PATH} \
 --load_checkpoint_id ${CHECKPOINT_EPOCH150_NAME} \
-&> 512_lamb_64GPU_80_2.log
+&> ${JOB_NAME}.log
