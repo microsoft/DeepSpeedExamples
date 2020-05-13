e0cc190a49f96f3b817296fe0427975a10ec0ea2
Reza Yazdani <reyazda@microsoft.com>
2020-05-13 16:24:43 +0000

    add deepspeed launcher at run_squad_deepspeed.sh

--- BingBertSquad/run_squad_deepspeed.sh
+++ BingBertSquad/run_squad_deepspeed.sh
@@ -11,7 +11,6 @@ SQUAD_DIR=$3
 OUTPUT_DIR=$4
 LR=${5:-0.00003}
 SEED=${6:-12345}
-config_json=$6
 MASTER_PORT=${7:-29500}
 echo "seed is $SEED"
 echo "master port is $MASTER_PORT"
@@ -27,10 +26,8 @@ else
        GRAD_ACCUM_STEPS=$((PER_GPU_BATCH_SIZE/MAX_GPU_BATCH_SIZE))
 fi
 JOB_NAME="deepspeed_${NGPU}GPUs_${EFFECTIVE_BATCH_SIZE}batch_size"
-#config_json=deepspeed_bsz24_config.json
-#run_cmd="deepspeed 
-run_cmd="python3.6 -m torch.distributed.launch \
-       --nproc_per_node=${NGPU} \
+config_json=deepspeed_bsz24_config.json
+run_cmd="deepspeed --num_nodes ${NUM_NODES} --num_gpus ${NGPU_PER_NODE} \
        --master_port=${MASTER_PORT} \
        nvidia_run_squad_deepspeed.py \
        --bert_model bert-large-uncased \
