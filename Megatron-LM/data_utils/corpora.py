75df8f6ce549aa92d78686564822164e4855c2d3
jren73 <renjie73@outlook.com>
2020-08-10 22:59:06 +0000

    modified:   Megatron-LM/data_utils/corpora.py 	modified:   Megatron-LM/scripts/ds_zero2_pretrain_gpt2_model_parallel_with_cpuoffload.sh

--- Megatron-LM/data_utils/corpora.py
+++ Megatron-LM/data_utils/corpora.py
@@ -22,7 +22,7 @@ class wikipedia(json_dataset):
 
 	command line usage: `--train-data wikipedia`
 	"""
-	PATH = '/turing-hdd/users/jieren/DeepSpeedExamples/Megatron-LM/openwebtext/train_data.json'
+	PATH = 'data/wikipedia/wikidump_lines.json'
 	assert_str = "make sure to set PATH for wikipedia data_utils/corpora.py"
 	def __init__(self, **kwargs):
 		assert os.path.exists(wikipedia.PATH), \
@@ -40,8 +40,7 @@ class webtext(json_dataset):
 
 	command line usage: `--train-data webtext`
 	"""
-	PATH = '/turing-hdd/users/jieren/DeepSpeedExamples/Megatron-LM/openwebtext/train_data.json'
-        #PATH = 'openwebtext/train_data.json'
+        PATH = 'data/webtext/data.json'
 	assert_str = "make sure to set PATH for webtext data_utils/corpora.py"
 	def __init__(self, **kwargs):
 		assert os.path.exists(webtext.PATH), \
