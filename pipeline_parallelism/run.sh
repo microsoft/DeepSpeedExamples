e1c8e4a525805e41f32d171630f7b444dc1d6681
Shaden Smith <shaden.smith@microsoft.com>
2020-09-09 13:11:24 +0000

    PP example code checkpoint

--- pipeline_parallelism/run.sh
+++ pipeline_parallelism/run.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-PP=2
-LOGFILE="results/cifar10-batch256-dp4-pp2-mb4-part_parameters.log"
+#"train_micro_batch_size_per_gpu" : 16,
 
-deepspeed -i worker-0@worker-1 train.py --deepspeed_config=ds_config.json -p 2 2>&1 --steps=20000 | tee ${LOGFILE}
+#deepspeed -i worker-0@worker-1 train.py --deepspeed_config=ds_config.json -p 2 2>&1 --steps=20000 | tee ${LOGFILE}
+GPUS=4
+for PP in 2 ;
+do
+    if [ $PP -gt 0 ];
+    then
+        DP=$(( ${GPUS} / ${PP} ))
+    else
+        DP=${GPUS}
+    fi
+
+    LOGFILE="results/cifar10-batch256-gpus${GPUS}-dp${DP}-pp${PP}-mb32.log"
+    deepspeed --num_nodes=1 train.py --deepspeed_config=ds_config.json -p ${PP} 2>&1 --steps=200 | tee ${LOGFILE}
+done
 
 deepspeed ~/train.py
--- pipeline_parallelism/train.py
+++ pipeline_parallelism/train.py
@@ -7,15 +7,16 @@ import torch.distributed as dist
 
 import torchvision
 import torchvision.transforms as transforms
-from torchvision.models.alexnet import AlexNet
-from torchvision.models.vgg import vgg19
+from torchvision.models import AlexNet
+from torchvision.models import vgg19
 
 import deepspeed
 from deepspeed.pipe import PipelineModule
+from deepspeed.utils import RepeatingLoader
 
 #from model import AlexNet
 
-def cifar_trainset(dl_path='/tmp/cifar10-data'):
+def cifar_trainset(local_rank, dl_path='/tmp/cifar10-data'):
 
     transform = transforms.Compose([
         transforms.Resize(256),
@@ -28,13 +29,13 @@ def cifar_trainset(dl_path='/tmp/cifar10-data'):
     # Note: if the download path is not on a shared filesytem, remove the semaphore
     # and switch to args.local_rank
     dist.barrier()
-    if dist.get_rank() != 0:
+    if local_rank != 0:
         dist.barrier()
     trainset = torchvision.datasets.CIFAR10(root=dl_path,
                                             train=True,
                                             download=True,
                                             transform=transform)
-    if dist.get_rank() == 0:
+    if local_rank == 0:
         dist.barrier()
     return trainset
 
@@ -71,7 +72,7 @@ def train_base(args):
     #net = AlexNet(num_classes=10)
     net = vgg19(num_classes=10)
 
-    trainset = cifar_trainset()
+    trainset = cifar_trainset(args.local_rank)
 
     engine, _, dataloader, __ = deepspeed.initialize(
         args=args,
@@ -79,12 +80,18 @@ def train_base(args):
         model_parameters=[p for p in net.parameters() if p.requires_grad],
         training_data=trainset)
     
+    dataloader = RepeatingLoader(dataloader)
+    data_iter = iter(dataloader)
+    
     rank = dist.get_rank()
     gas = engine.gradient_accumulation_steps()
 
     criterion = torch.nn.CrossEntropyLoss()
 
-    for idx, batch in enumerate(dataloader):
+    total_steps = args.steps * engine.gradient_accumulation_steps()
+    step = 0
+    for micro_step in range(total_steps):
+        batch = next(data_iter)
         inputs = batch[0].to(engine.device)
         labels = batch[1].to(engine.device)
 
@@ -93,9 +100,8 @@ def train_base(args):
         engine.backward(loss)
         engine.step()
 
-        micro_batches = idx+1
-        if micro_batches % engine.gradient_accumulation_steps() == 0:
-            step = micro_batches // engine.gradient_accumulation_steps()
+        if micro_step % engine.gradient_accumulation_steps() == 0:
+            step += 1
             if rank == 0 and (step % 10 == 0):
                 print(f'step: {step:3d} / {args.steps:3d} loss: {loss}')
             if step == args.steps:
@@ -103,27 +109,27 @@ def train_base(args):
 
             
 
-def to_layers(vision_model):
+def join_layers(vision_model):
     layers = [
         *vision_model.features,
         vision_model.avgpool,
         lambda x: torch.flatten(x, 1),
-        *vision_model.classifier
+        *vision_model.classifier,
     ]
     return layers
 
 
 def train_pipe(args, part='parameters'):
-    #net = AlexNet(num_classes=10)
     torch.manual_seed(args.seed)
+    #net = AlexNet(num_classes=10)
     net = vgg19(num_classes=10)
-    net = PipelineModule(layers=to_layers(net),
+    net = PipelineModule(layers=join_layers(net),
                          loss_fn=torch.nn.CrossEntropyLoss(),
                          num_stages=args.pipeline_parallel_size,
                          partition_method=part,
                          activation_checkpoint_interval=0)
     
-    trainset = cifar_trainset()
+    trainset = cifar_trainset(args.local_rank)
     engine, _, _, _ = deepspeed.initialize(
         args=args,
         model=net,
@@ -146,9 +152,10 @@ if __name__ == '__main__':
     torch.cuda.set_device(args.local_rank)
     dist.init_process_group(backend=args.backend)
 
-    #train_base(args)
+    if args.pipeline_parallel_size == 0:
+        train_base(args)
+    else:
+        train_pipe(args, part='parameters')
 
     for part in ['parameters']:
-        train_pipe(args, part=part)
-    '''
-    '''
+        pass
