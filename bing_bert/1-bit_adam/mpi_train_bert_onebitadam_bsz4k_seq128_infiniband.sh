4944d5f9324409d4d66d7fa39db5011ec2caa011
Conglong Li <conglong.li@gmail.com>
2021-02-18 16:36:00 -0800

    update mvapich flags

--- bing_bert/1-bit_adam/mpi_train_bert_onebitadam_bsz4k_seq128_infiniband.sh
+++ bing_bert/1-bit_adam/mpi_train_bert_onebitadam_bsz4k_seq128_infiniband.sh
@@ -14,7 +14,8 @@ OUTPUT_DIR=${base_dir}/bert_model_outputs
 
 mkdir -p $OUTPUT_DIR
 
-mpirun -n 128 -ppn 8 -f /tmp/deepspeed_mvapich_hostfile -env MV2_SUPPORT_DL=1 python ${base_dir}/../deepspeed_train.py \
+# LD_PRELOAD is used to load a specific nccl version. Remove it if needed.
+mpirun -n 128 -ppn 8 -f /tmp/deepspeed_mvapich_hostfile -env MV2_SMP_USE_CMA=0 -env MV2_DEBUG_SHOW_BACKTRACE=1 -env MV2_USE_CUDA=1 -env MV2_SUPPORT_DL=1 -env MV2_ENABLE_AFFINITY=0 -env MV2_INTER_ALLGATHER_TUNING=5 -env MV2_CUDA_USE_NAIVE=0 -env NCCL_TREE_THRESHOLD=0 -env PYTHONUNBUFFERED=True -env MV2_USE_GDRCOPY=0 -env MV2_USE_GDR=0 -env NCCL_VERSION=2.8.3 -env PYTHON_VERSION=3 -env LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libnccl.so.2.8.3 python ${base_dir}/../deepspeed_train.py \
 --cf ${base_dir}/../bert_large.json \
 --max_seq_length 128 \
 --output_dir $OUTPUT_DIR \
