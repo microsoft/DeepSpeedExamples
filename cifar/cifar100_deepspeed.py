36046a17ee5600136cbf306041d19f1ecdb75b98
Conglong Li <conglong.li@gmail.com>
2022-06-08 23:38:33 -0700

    fix

--- cifar/cifar100_deepspeed.py
+++ cifar/cifar100_deepspeed.py
@@ -1,6 +1,6 @@
 import torch
 import torch.distributed as dist
-from tensorboardX import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 import torchvision
 import torchvision.transforms as transforms
 import argparse
@@ -90,6 +90,16 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
+if master_process(args):
+    trainset = torchvision.datasets.CIFAR100(root='./data',
+                                             train=True,
+                                             download=True,
+                                             transform=transform_train)
+    testset = torchvision.datasets.CIFAR100(root='./data',
+                                            train=False,
+                                            download=True,
+                                            transform=transform_test)
+dist.barrier()
 trainset = torchvision.datasets.CIFAR100(root='./data',
                                          train=True,
                                          download=True,
