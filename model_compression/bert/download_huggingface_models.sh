fc841ce0127ce5e4d3fdfcacca669801ce2cacde
xiaoxiawu <xiaoxiawu@microsoft.com>
2022-07-08 22:07:33 +0000

    clean

--- model_compression/bert/download_huggingface_models.sh
+++
@@ -1,5 +0,0 @@
-# yoshitomo-matsubara/bert-large-uncased-
-for task in  mrpc #sst2 stsb mnli qqp rte cola mrpc qnli
-do
-git lfs clone https://huggingface.co/yoshitomo-matsubara/bert-base-uncased-$task &
-done
