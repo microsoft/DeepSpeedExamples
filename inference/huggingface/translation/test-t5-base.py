71f274b71d3f4fbd02878c5cee0a97d0d4a6a32d
Lev Kurilenko <lekurile@microsoft.com>
2022-10-07 23:15:05 +0000

    test-t5-base.py injection policy

--- inference/huggingface/translation/test-t5-base.py
+++ inference/huggingface/translation/test-t5-base.py
@@ -2,6 +2,7 @@ import deepspeed
 import torch
 import os
 from transformers import pipeline
+from transformers.models.t5.modeling_t5 import T5Block
 
 local_rank = int(os.getenv('LOCAL_RANK', '0'))
 world_size = int(os.getenv('WORLD_SIZE', '4'))
@@ -11,9 +12,10 @@ translator = pipeline("translation_en_to_fr", model="t5-base", tokenizer="t5-bas
 
 # DeepSpeed init_inference API
 translator.model = deepspeed.init_inference(translator.model,
-                                           mp_size=world_size,
-                                           dtype=torch.float16,
-                                           replace_with_kernel_inject=True)
+    mp_size=world_size,
+    dtype=torch.float,
+    injection_policy={T5Block: ('SelfAttention.o', 'EncDecAttention.o', 'DenseReluDense.wo')}
+)
 
 # Translate text
 text = "The quick brown fox jumps over the lazy dog."
