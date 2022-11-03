64ba79dbe6a187db0f846b0621530eb49397b57d
Molly Smith <112220543+molly-smith@users.noreply.github.com>
2022-11-03 16:04:21 -0700

    Inference test for codegen

---
+++ inference/huggingface/text-generation/test-codegen.py
@@ -0,0 +1,29 @@
+from transformers import pipeline
+import transformers
+import deepspeed
+import torch
+import os
+from transformers.models.codegen.modeling_codegen import CodeGenBlock
+
+local_rank = int(os.getenv('LOCAL_RANK', '0'))
+world_size = int(os.getenv('WORLD_SIZE', '1'))
+
+pipe = pipeline("text-generation", model="Salesforce/codegen-350M-mono", device=local_rank)
+
+# The injection_policy shows two things:
+#   1. which layer module we need to add Tensor-Parallelism
+#   2. the name of several linear layers: a) attention_output (both encoder and decoder),
+#       and b) transformer output
+
+pipe.model = deepspeed.init_inference(
+    pipe.model,
+    mp_size=world_size,
+    dtype=torch.float,
+    injection_policy={CodeGenBlock: ('mlp.fc_out', 'attn.out_proj')}
+)
+
+pipe.device = torch.device(f'cuda:{local_rank}')
+output = pipe("def hello_world():")
+
+if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
+    print(output)
