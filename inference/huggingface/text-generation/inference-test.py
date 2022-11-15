c0db7e37a4cc73226986606e945a6760ea60e716
molly-smith <mosm@microsoft.com>
2022-11-15 01:53:27 +0000

    Add profiling to inference test

--- inference/huggingface/text-generation/inference-test.py
+++ inference/huggingface/text-generation/inference-test.py
@@ -4,6 +4,7 @@ import deepspeed
 import math
 import os
 import torch
+import time
 from utils import Pipeline
 
 
@@ -20,6 +21,36 @@ parser.add_argument("--use_meta_tensor", default=False, type=bool, help="use the
 parser.add_argument("--use_cache", default=True, type=bool, help="use cache for generation")
 parser.add_argument("--local_rank", default=0, type=int, help=())
 args = parser.parse_args()
+
+def print_latency(latency_set, title, warmup=3):
+    # trim warmup queries
+    latency_set = list(latency_set)
+    latency_set = latency_set[warmup:]
+    count = len(latency_set)
+    print(latency_set)
+    if count > 0:
+        latency_set.sort()
+        n50 = (count - 1) * 0.5 + 1
+        n90 = (count - 1) * 0.9 + 1
+        n95 = (count - 1) * 0.95 + 1
+        n99 = (count - 1) * 0.99 + 1
+        n999 = (count - 1) * 0.999 + 1
+
+        avg = sum(latency_set) / count
+        p50 = latency_set[int(n50) - 1]
+        p90 = latency_set[int(n90) - 1]
+        p95 = latency_set[int(n95) - 1]
+        p99 = latency_set[int(n99) - 1]
+        p999 = latency_set[int(n999) - 1]
+
+        print(f"====== latency stats {title} ======")
+        print("\tAvg Latency: {0:8.2f} ms".format(avg * 1000))
+        print("\tP50 Latency: {0:8.2f} ms".format(p50 * 1000))
+        print("\tP90 Latency: {0:8.2f} ms".format(p90 * 1000))
+        print("\tP95 Latency: {0:8.2f} ms".format(p95 * 1000))
+        print("\tP99 Latency: {0:8.2f} ms".format(p99 * 1000))
+        print("\t999 Latency: {0:8.2f} ms".format(p999 * 1000))
+
 
 local_rank = int(os.getenv('LOCAL_RANK', '0'))
 world_size = int(os.getenv('WORLD_SIZE', '1'))
@@ -38,6 +69,11 @@ if args.ds_inference:
                                     replace_with_kernel_inject=True,
                                     max_tokens=args.max_tokens
                                     )
+    pipe.model.profile_model_time()
+
+responses = []
+times = []
+mtimes = []
 
 input_sentences = [
          "DeepSpeed is a machine learning framework",
@@ -56,11 +92,28 @@ if args.batch_size > len(input_sentences):
 
 inputs = input_sentences[:args.batch_size]
 
-outputs = pipe(inputs, 
-              num_tokens=args.max_new_tokens, 
-              do_sample=(not args.greedy)
-              )
+for i in range(30):
+    torch.cuda.synchronize()
+    start = time.time()
+
+    outputs = pipe(inputs, 
+                num_tokens=args.max_new_tokens, 
+                do_sample=(not args.greedy)
+                )
+
+    torch.cuda.synchronize()
+    end = time.time()
+
+    times.append(end - start)  # / (args.max_tokens - 3))
+    mtimes.append(sum(pipe.model.model_times()))
+
 
 for i, o in zip(inputs, outputs):
     print(f"\nin={i}\nout={o}\n{'-'*60}")
 
+if args.local_rank == 0:
+    print_latency(times, "(e2e) latency")
+    print_latency(mtimes, "(model-only) latency")
+    print_latency(map(lambda t: t / (args.max_tokens - 3),
+                      times),
+                  "(e2e) per token latency")
