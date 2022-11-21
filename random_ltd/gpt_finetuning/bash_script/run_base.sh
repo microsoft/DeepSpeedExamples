29ab56eb79d3f409a58089208c93a25420cd8271
xiaoxiawu-microsfot <xiaoxiawu@microsoft.com>
2022-11-22 04:20:42 +0500

    refacrtor

--- random_ltd/gpt_finetuning/bash_script/run_base.sh
+++ random_ltd/gpt_finetuning/bash_script/run_base.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-##################fine-tune the origin model and then apply zeroquant, the following command will take approximately 10 mins in A100
-###zero-quant https://arxiv.org/abs/2206.01861
+##################apply random-ltd to fine-tune ptb on GPT-base (12-layer)##############################
 export CUDA_VISIBLE_DEVICES=1
-
-######### fp16
-# python -m torch.distributed.launch --nproc_per_node=1 \
-#     --master_port 12345 \
-#     run_clm_no_trainer.py \
-#     --dataset_name ptb_text_only \
-#     --dataset_config_name penn_treebank \
-#     --model_name_or_path gpt2 \
-#     --per_device_train_batch_size 4 \
-#     --num_train_epochs 2 \
-#     --deepspeed_config config/ds_config_old.json \
-#     --deepspeed --seed 1234\
-#     --output_dir ./output/check | tee -a ./output/check/train_before2.log
-
-# export CUDA_VISIBLE_DEVICES=0
-# mkdir output1/check
-# ######### fp16
+mkdir -p ./output/check_base
 python -m torch.distributed.launch --nproc_per_node=1 \
     --master_port 12346 \
     run_clm_no_trainer.py \
@@ -30,4 +13,4 @@ python -m torch.distributed.launch --nproc_per_node=1 \
     --num_train_epochs 2 \
     --deepspeed_config config/ds_config_gpt_base.json \
     --deepspeed --seed 1234\
-    --output_dir ./output2/check #&> ./output2/check/train_with_real_kernel.log # ./output1/check/train_with_kernel_fp16.log
+    --output_dir ./output/check_base #&> ./output/check_base/training.log 
