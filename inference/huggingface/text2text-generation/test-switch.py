00a790093522aca44952d3331d3da0ac5f461c08
Molly Smith <mosm@microsoft.com>
2023-01-24 22:33:35 +0000

    switch stack

--- inference/huggingface/text2text-generation/test-switch.py
+++ inference/huggingface/text2text-generation/test-switch.py
@@ -1,24 +1,30 @@
-from transformers import pipeline
+from transformers import pipeline, T5Tokenizer, SwitchTransformersModel, SwitchTransformersForConditionalGeneration
 import transformers
 import deepspeed
 import torch
 import os
-from transformers.models.switch_transformers.modeling_switch_transformers import SwitchTransformersBlock
+from transformers.models.switch_transformers.modeling_switch_transformers import SwitchTransformersBlock, SwitchTransformersStack
 
 local_rank = int(os.getenv('LOCAL_RANK', '0'))
 world_size = int(os.getenv('WORLD_SIZE', '1'))
 
-pipe = pipeline("text2text-generation", model="google/switch-base-8", device=local_rank)
+tokenizer = T5Tokenizer.from_pretrained("google/switch-base-8")
+model = SwitchTransformersForConditionalGeneration.from_pretrained("google/switch-base-8")
+
+pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer, device=local_rank)
 
 pipe.model = deepspeed.init_inference(
     pipe.model,
     mp_size=world_size,
-    dtype=torch.float,
-    injection_policy={SwitchTransformersBlock: ('SelfAttention.o', 'EncDecAttention.o', 'mlp.wo', 
-        'expert_0.wo','expert_1.wo','expert_2.wo','expert_3.wo','expert_4.wo','expert_5.wo','expert_6.wo', 'expert_7.wo')}
+    dtype=torch.float16,
+    injection_policy={SwitchTransformersStack: ('SelfAttention.o', 'EncDecAttention.o', 'mlp.wo', 
+        #'router.classifier',
+        'expert_0.wo','expert_1.wo','expert_2.wo','expert_3.wo','expert_4.wo','expert_5.wo','expert_6.wo', 
+        'expert_7.wo'
+        )}
 )
 
-print(pipe.model)
+#print(pipe.model)
 
 pipe.device = torch.device(f'cuda:{local_rank}')
 output = pipe("summarize: Studies have been shown that owning a dog is good for you")
