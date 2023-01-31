bd22ca4d7450177505dbe729649066df68c0fc15
molly-smith <mosm@microsoft.com>
2023-01-31 05:36:59 +0000

    Add batching (has bad generated outputs)

--- inference/huggingface/text-generation/test-accelerate.py
+++ inference/huggingface/text-generation/test-accelerate.py
@@ -1,4 +1,5 @@
 import os
+import math
 from accelerate import init_empty_weights, load_checkpoint_and_dispatch
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 from huggingface_hub import snapshot_download
@@ -16,8 +17,28 @@ model = load_checkpoint_and_dispatch(
     model, weights_path, device_map="auto", no_split_module_classes=["GPTJBlock"]
 )
 
+input_sentences = [
+         "He is working on",
+         "DeepSpeed is a machine learning framework",
+         "He got all",
+         "Everyone is happy and I can",
+         "The new movie that got Oscar this year",
+         "In the far far distance from our galaxy,",
+         "Peace is the only way"
+]
+
+batch_size = 3
+
+if batch_size > len(input_sentences):
+    # dynamically extend to support larger bs by repetition
+    input_sentences *= math.ceil(batch_size / len(input_sentences))
+
+inputs = input_sentences[:batch_size]
+
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-inputs = tokenizer("Hello, my name is", return_tensors="pt")
-inputs = inputs.to(0)
-output = model.generate(inputs["input_ids"])
-print(tokenizer.decode(output[0].tolist()))
+tokenizer.pad_token = tokenizer.eos_token
+inputs = tokenizer(inputs, return_tensors="pt", padding=True)
+
+output = model.generate(inputs["input_ids"].to(0))
+outputs = tokenizer.batch_decode(output, skip_special_tokens=True)
+print(outputs)
