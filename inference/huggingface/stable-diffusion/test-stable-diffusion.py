6fac0daaf527764111cbbebf70180e2af4e0eae0
PareesaMS <pagolnar@microsoft.com>
2023-04-06 23:19:19 +0000

    Add local_pipeline

--- inference/huggingface/stable-diffusion/test-stable-diffusion.py
+++ inference/huggingface/stable-diffusion/test-stable-diffusion.py
@@ -1,8 +1,9 @@
 import deepspeed
 import torch
 import os
-
+import pdb
 from diffusers import DiffusionPipeline
+import local_pipeline_stable_diffusion
 
 prompt = "a dog on a rocket"
 
@@ -12,12 +13,13 @@ device = torch.device(f"cuda:{local_rank}")
 world_size = int(os.getenv('WORLD_SIZE', '1'))
 generator = torch.Generator(device=torch.cuda.current_device())
 
+pdb.set_trace()
 pipe = DiffusionPipeline.from_pretrained(model, torch_dtype=torch.half)
 pipe = pipe.to(device)
 
 generator.manual_seed(0xABEDABE7)
 baseline_image = pipe(prompt, guidance_scale=7.5, generator=generator).images[0]
-baseline_image.save(f"baseline.png")
+#baseline_image.save(f"baseline.png")
 
 # NOTE: DeepSpeed inference supports local CUDA graphs for replaced SD modules.
 #       Local CUDA graphs for replaced SD modules will only be enabled when `mp_size==1`
