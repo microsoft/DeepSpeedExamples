cd19b3bf1e5b60dd73b09c7463da4eedada1eed7
Li Ding <3971036+0xDing@users.noreply.github.com>
2023-04-14 16:03:42 +0800

    [DeepSpeed-Chat] feat: Add support for single-response datasets during SFT training phase (#270)
    * feat: add sft only dataset support
    
    * fix: cache and shuffle
    
    * fix: code style
    
    * fix: code style
    
    ---------
    
    Co-authored-by: Li (Boris) Ding <ding@pleisto.com>

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
@@ -50,6 +50,11 @@ def parse_args():
                         'will use 60% of data for phase 1, 20% for phase 2'
                         'and 20% for phase 3.')
     parser.add_argument(
+        '--sft_only_data_path',
+        nargs='*',
+        default=[],
+        help='Path to the dataset for only using in SFT phase.')
+    parser.add_argument(
         '--data_output_path',
         type=str,
         default='/tmp/data_files/',
@@ -211,9 +216,15 @@ def main():
     # Prepare the data
     train_phase = 1
     train_dataset, eval_dataset = create_prompt_dataset(
-        args.local_rank, args.data_path, args.data_split,
-        args.data_output_path, train_phase, args.seed, tokenizer,
-        args.max_seq_len)
+        args.local_rank,
+        args.data_path,
+        args.data_split,
+        args.data_output_path,
+        train_phase,
+        args.seed,
+        tokenizer,
+        args.max_seq_len,
+        sft_only_data_path=args.sft_only_data_path)
 
     # DataLoaders creation:
     if args.local_rank == -1:
