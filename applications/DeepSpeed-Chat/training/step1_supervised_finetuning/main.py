dfb9491774d1848304fd424556ee5bfe43b7d293
LIU Wangsheng <RobinSeaside@users.noreply.github.com>
2023-04-25 00:32:48 +0800

    Update main.py (#399)
    Co-authored-by: Zhewei Yao <zheweiy@berkeley.edu>

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
@@ -46,7 +46,7 @@ def parse_args():
                         type=str,
                         default='6,2,2',
                         help='Comma-separated list of proportions for training'
-                        'phase 1, 2, and 3 data. For example the split `2,4,4`'
+                        'phase 1, 2, and 3 data. For example the split `6,2,2`'
                         'will use 60% of data for phase 1, 20% for phase 2'
                         'and 20% for phase 3.')
     parser.add_argument(
