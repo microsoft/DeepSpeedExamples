794660ffa77ad4c6ddc9bc68c7ecefd5e35793a7
Conglong Li <conglong.li@gmail.com>
2023-04-23 21:23:28 -0700

    fix old scripts (#408)
    * fix old scripts
    
    * format

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/other_language/run_japanese.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/other_language/run_japanese.sh
@@ -3,22 +3,34 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # DeepSpeed Team
-MODEL=sberbank-ai/mGPT
-OUTPUT_PATH=./output_japanese_mGPT_1.3b_4data
-mkdir -p $OUTPUT_PATH
+OUTPUT=$1
+ZERO_STAGE=$2
+if [ "$OUTPUT" == "" ]; then
+    OUTPUT=./output
+fi
+if [ "$ZERO_STAGE" == "" ]; then
+    ZERO_STAGE=2
+fi
+mkdir -p $OUTPUT
+
 # The Japanese data we found mostly only contain one response without another
 # "rejected" response. Thus we only test the step 1 finetuning and use
 # a data_split of 10,0,0 (keep all data for step 1).
 deepspeed main.py \
    --data_path mkqa-Japanese Cohere/miracl-ja-queries-22-12 lmqg/qg_jaquad lmqg/qag_jaquad \
    --data_split 10,0,0 \
-   --model_name_or_path $MODEL \
-   --per_device_train_batch_size 2 \
-   --per_device_eval_batch_size 2 \
+   --model_name_or_path sberbank-ai/mGPT \
+   --per_device_train_batch_size 8 \
+   --per_device_eval_batch_size 8 \
+   --max_seq_len 512 \
    --learning_rate 9.65e-6 \
-   --num_train_epochs 16  \
-   --deepspeed_config ./ds_config.json \
-   --deepspeed --seed 1234 --num_warmup_steps 0 \
+   --weight_decay 0. \
+   --num_train_epochs 16 \
+   --gradient_accumulation_steps 1 \
    --lr_scheduler_type cosine \
-   --output_dir $OUTPUT_PATH \
-   &> $OUTPUT_PATH/training.log
+   --num_warmup_steps 0 \
+   --seed 1234 \
+   --zero_stage $ZERO_STAGE \
+   --deepspeed \
+   --output_dir $OUTPUT \
+   &> $OUTPUT/training.log
