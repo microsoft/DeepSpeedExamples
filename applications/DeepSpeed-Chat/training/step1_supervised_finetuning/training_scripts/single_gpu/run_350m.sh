4b8ac73cc82c310b6266f5e8b8e18f0b4d9908e9
mtax <mtax687@gmail.com>
2023-05-06 13:59:14 +0800

    load pretrained model from local path

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_gpu/run_350m.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_gpu/run_350m.sh
@@ -16,6 +16,11 @@ OUTPUT=${1:-output}
 ZERO_STAGE=${2:-0}
 mkdir -p $OUTPUT
 
-deepspeed --num_gpus 1 main.py --model_name_or_path facebook/opt-1.3b \
-   --gradient_accumulation_steps 8 --lora_dim 128 --zero_stage $ZERO_STAGE \
-   --deepspeed --output_dir $OUTPUT &> $OUTPUT/training.log
+run_cmd="deepspeed --num_gpus 1 main.py \
+  --model_name_or_path ${MODEL_STORAGE}facebook/opt-350m \
+   --gradient_accumulation_steps 8 \
+   --lora_dim 128 --zero_stage $ZERO_STAGE \
+   --deepspeed --output_dir $OUTPUT &> $OUTPUT/training.log"
+
+echo ${run_cmd}
+eval ${run_cmd}
