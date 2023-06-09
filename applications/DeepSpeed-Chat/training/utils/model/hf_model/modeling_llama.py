4ff99779785e6185aaf1b0961799a8dda940f0c6
xiaoxiawu <xiaoxiawu@microsoft.com>
2023-06-09 10:34:13 +0500

    fix gpu and cpu error

--- applications/DeepSpeed-Chat/training/utils/model/hf_model/modeling_llama.py
+++ applications/DeepSpeed-Chat/training/utils/model/hf_model/modeling_llama.py
@@ -199,7 +199,9 @@ class LlamaAttention(nn.Module):
         if past_key_value is not None:
             kv_seq_len += past_key_value[0].shape[-2]
         cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-        
+        if cos.device != query_states.device:
+            cos = cos.to(query_states.device)
+            sin = sin.to(query_states.device)
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
         # [bsz, nh, t, hd]
 
