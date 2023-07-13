1d1925a7b3239300f864cada96580105e5d97f78
Lev Kurilenko <lekurile@microsoft.com>
2023-07-13 23:44:25 +0000

    Remove explicit GPU arg in lora script

--- applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/single_node/sweep/run_1.3b_lora_swp.sh
+++ applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/single_node/sweep/run_1.3b_lora_swp.sh
@@ -39,7 +39,7 @@ Num_Padding_at_Beginning=1 # this is model related
 Actor_Lr=9.65e-6
 Critic_Lr=5e-6
 
-cmd="deepspeed --num_nodes=1 --num_gpus=16 main.py \
+cmd="deepspeed --num_nodes=1 main.py \
    --data_path Dahoas/rm-static \
    --data_split 2,4,4 \
    --actor_model_name_or_path $ACTOR_MODEL_PATH \
