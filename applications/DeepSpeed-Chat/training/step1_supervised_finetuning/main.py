fa500d784e7e502d2327f4ef4e705867bd194940
Tunji Ruwase <olruwase@microsoft.com>
2023-09-25 22:21:06 +0500

    Format fixes

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
@@ -146,9 +146,11 @@ def parse_args():
     parser.add_argument('--offload',
                         action='store_true',
                         help='Enable ZeRO Offload techniques.')
-    parser.add_argument('--dtype', type=str, default='fp16',
+    parser.add_argument('--dtype',
+                        type=str,
+                        default='fp16',
                         choices=['fp16', 'bf16'],
-                        help = 'Training data type')
+                        help='Training data type')
     parser.add_argument(
         '--zero_stage',
         type=int,
