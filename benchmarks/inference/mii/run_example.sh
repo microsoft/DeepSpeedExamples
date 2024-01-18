a1eb29d263a0615f1e7c05c0ae668b7014d68313
Michael Wyatt <michaelwyatt@microsoft.com>
2024-01-18 14:21:18 -0800

    update example script

--- benchmarks/inference/mii/run_example.sh
+++ benchmarks/inference/mii/run_example.sh
@@ -1,24 +1,23 @@
 ### Run the server
+echo "Starting the server"
 python server.py \
         --model meta-llama/Llama-2-7b-hf \
-        --deployment_name llama2-7b-tp1-b768 \
         --tp_size 1 \
         --max_ragged_batch_size 768 \
         start
 
 ### This command will run the client with 60 generation steps and input prompt length of 2600
+echo "Running the client"
 python client.py \
-        --deployment_name llama2-7b-tp1-b768 \
         --mean_prompt_length 2600 \
         --mean_max_new_tokens 60
 
 ### Stop the server
-echo "Stopping server"
-python server.py -d ${DEPLOYMENT_NAME} stop
+echo "Stopping the server"
+python server.py stop
 sleep 120
 
 ### Gernerate the plots
-python plot_th_lat.py --log_dir . --test --no_vllm
-python plot_effective_throughput.py --log_dir . --test --no_vllm
+python plot_th_lat.py
 
-echo "Find the plots in the charts directory and the logs inside logs.llama2-7b-tp1-b768"
+echo "Find figures in ./plots/ and log outputs in ./results/"
