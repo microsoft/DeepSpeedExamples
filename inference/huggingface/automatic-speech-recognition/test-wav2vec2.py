26fccd1c567523ff2cfbee80e0fb6804aeed55ab
Wang, Zixu <zixu.wang@intel.com>
2024-01-31 01:14:56 -0800

    test xpu in local

--- inference/huggingface/automatic-speech-recognition/test-wav2vec2.py
+++ inference/huggingface/automatic-speech-recognition/test-wav2vec2.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import soundfile as sf
 import torch
@@ -7,27 +7,34 @@ import os
 import deepspeed
 from deepspeed import module_inject
 from transformers.models.wav2vec2.modeling_wav2vec2 import Wav2Vec2EncoderLayer
+from deepspeed.accelerator import get_accelerator
 
-librispeech_eval = load_dataset("librispeech_asr", "clean", split="test")
+# librispeech_eval = load_dataset("librispeech_asr", "clean", split="test")
+data_path = "~/my_project/DSE/HuggingFace/DataSets/librispeech_asr"
+librispeech_eval = load_from_disk(data_path)
 
 # Get local gpu rank from torch.distributed/deepspeed launcher
 local_rank = int(os.getenv('LOCAL_RANK', '0'))
 world_size = int(os.getenv('WORLD_SIZE', '1'))
+device = torch.device(get_accelerator().device_name(local_rank))
 
 print(
     "***************** Creating model in RANK ({0}) with WORLD_SIZE = {1} *****************"
     .format(local_rank,
             world_size))
 
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
-processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+# model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+# processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+model_path = "/home/zixuwang/my_project/DSE/HuggingFace/Models/wav2vec2"
+model = Wav2Vec2ForCTC.from_pretrained(model_path)
+processor = Wav2Vec2Processor.from_pretrained(model_path)
 
 model = deepspeed.init_inference(model,
                                  mp_size=world_size,
                                  dtype=torch.float,
                                  injection_policy={Wav2Vec2EncoderLayer: ('attention.out_proj','feed_forward.output_dense')},
                                  replace_with_kernel_inject=False)
-model.to(f'cuda:{local_rank}')
+model.to(device)
 def map_to_array(batch):
     speech, _ = sf.read(batch["file"])
     batch["speech"] = speech
@@ -38,7 +45,7 @@ librispeech_eval = librispeech_eval.map(map_to_array)
 def map_to_pred(batch):
     input_values = processor(batch["speech"], return_tensors="pt", padding="longest").input_values
     with torch.no_grad():
-        logits = model(input_values.to(f'cuda:{local_rank}')).logits
+        logits = model(input_values.to(device)).logits
 
     predicted_ids = torch.argmax(logits, dim=-1)
     transcription = processor.batch_decode(predicted_ids)
