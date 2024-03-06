34388aec9aefb690cb07cd3fe9ab11215c7f06ae
xiaoxiawu-microsoft <xiaoxiawu@microsoft.com>
2024-03-06 02:52:17 +0000

    update tpsize

--- benchmarks/inference/mii/run_fp6.sh
+++ benchmarks/inference/mii/run_fp6.sh
@@ -3,8 +3,8 @@
 
 # DeepSpeed Team
 
-MODELS=(NousResearch/Llama-2-13b-hf)
+MODELS=(NousResearch/Llama-2-70b-hf)
 
 for MODEL in ${MODELS[@]}; do
-    python ./run_benchmark.py --model ${MODEL} --num_requests 128 --stream --backend fastgen --fp6
+    python ./run_benchmark.py --model ${MODEL} --num_requests 128 --stream --backend fastgen --fp6  --tp_size 1
 done
