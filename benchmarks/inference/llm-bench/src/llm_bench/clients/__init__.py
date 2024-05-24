bc644708f4097035a5780bd567f419430b2e7314
Michael Wyatt <michaelwyatt@microsoft.com>
2024-05-24 11:37:41 -0700

    small change

--- benchmarks/inference/llm-bench/src/llm_bench/clients/__init__.py
+++ benchmarks/inference/llm-bench/src/llm_bench/clients/__init__.py
@@ -1,10 +1,10 @@
-from .azure_ml_client import AzureMLClientConfig, AzureMLClient
 from .base import BaseClient
+
+from .azure_ml_client import AzureMLClientConfig, AzureMLClient
 from .dummy_client import DummyClientConfig, DummyClient
 from .fastgen_client import FastGenClientConfig, FastGenClient
 from .vllm_client import vLLMClientConfig, vLLMClient
 
-# TODO: Make these auto-generated, so new clients can be added without changing this file
 client_config_classes = {
     "dummy": DummyClientConfig,
     "azure_ml": AzureMLClientConfig,
