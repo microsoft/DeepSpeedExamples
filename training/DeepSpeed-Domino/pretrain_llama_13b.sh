eccdf3861aae8e56fefb9d472f4874a30b407b2a
zhangsmallshark <zhangsmallshark@gmail.com>
2024-10-29 14:09:33 -0500

    train bash

--- training/DeepSpeed-Domino/pretrain_llama_13b.sh
+++ training/DeepSpeed-Domino/pretrain_llama_13b.sh
@@ -15,13 +15,12 @@ WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
  
 CHECKPOINT_PATH=checkpoint
 rm -rf $CHECKPOINT_PATH/*
-
 VOCAB_FILE="dataset/gpt2-vocab.json"
 MERGE_FILE="dataset/gpt2-merges.txt"
 DATA_PATH="dataset/my-gpt2_text_document"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-# export PYTHONPATH=$SCRIPT_DIR:$PYTHONPATH
+export PYTHONPATH=$SCRIPT_DIR:$PYTHONPATH
  
 DISTRIBUTED_ARGS="
     --nproc_per_node $GPUS_PER_NODE \
@@ -30,67 +29,36 @@ DISTRIBUTED_ARGS="
     --master_addr $MASTER_ADDR \
     --master_port $MASTER_PORT
 "
- 
-# LLaMA2 7b
-#    --no-async-tensor-model-parallel-allreduce \
-#     --disable-bias-linear \
+
 LLAMA_ARGS="
-    --num-layers 32 \
+    --llama-model \
+    --num-layers 2 \
     --hidden-size 4096 \
     --num-attention-heads 32 \
     --seq-length 1024 \
     --max-position-embeddings 1024 \
     --position-embedding-type rope \
     --swiglu \
-    --ffn-hidden-size 11008\
+    --ffn-hidden-size 11008 \
+    --disable-bias-linear \
     --normalization RMSNorm \
     --layernorm-epsilon 1e-6 \
-    --micro-batch-size 16 \
-    --global-batch-size 16 \
+    --micro-batch-size 8 \
+    --global-batch-size 8 \
     --lr 0.00015 \
-    --train-iters 80 \
+    --train-iters 100 \
     --lr-decay-iters 320000 \
     --lr-decay-style cosine \
     --min-lr 1.0e-5 \
     --weight-decay 1e-2 \
     --lr-warmup-fraction .01 \
     --clip-grad 1.0 \
+    --no-gradient-accumulation-fusion \
     --fp16 \
     --tensor-model-parallel-size $WORLD_SIZE \
-    --seed 3407 
+    --seed 3407 \
+    --causal-lm
 "
-
-# llama2-13b
-# LLAMA_ARGS="
-#     --llama-model \
-#     --num-layers 2 \
-#     --hidden-size 4096 \
-#     --num-attention-heads 32 \
-#     --seq-length 1024 \
-#     --max-position-embeddings 1024 \
-#     --position-embedding-type rope \
-#     --swiglu \
-#     --ffn-hidden-size 11008 \
-#     --disable-bias-linear \
-#     --normalization RMSNorm \
-#     --layernorm-epsilon 1e-6 \
-#     --micro-batch-size 8 \
-#     --global-batch-size 8 \
-#     --lr 0.00015 \
-#     --train-iters 100 \
-#     --lr-decay-iters 320000 \
-#     --lr-decay-style cosine \
-#     --min-lr 1.0e-5 \
-#     --weight-decay 1e-2 \
-#     --lr-warmup-fraction .01 \
-#     --clip-grad 1.0 \
-#     --no-gradient-accumulation-fusion \
-#     --fp16 \
-#     --tensor-model-parallel-size $WORLD_SIZE \
-#     --seed 3407 \
-#     --causal-lm
-# "
-
 
 DATA_ARGS="
     --data-path $DATA_PATH \
@@ -105,15 +73,12 @@ OUTPUT_ARGS="
     --eval-interval 1000 \
     --eval-iters 1
 "
- 
-# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun $DISTRIBUTED_ARGS pretrain_llama.py \
+
 cmd="deepspeed --num_gpus $WORLD_SIZE \
     pretrain_llama.py \
     $LLAMA_ARGS \
     $DATA_ARGS \
     $OUTPUT_ARGS 
     "
-    # --save $CHECKPOINT_PATH \
-    # --load $CHECKPOINT_PATH
 echo $cmd
 eval $cmd 
